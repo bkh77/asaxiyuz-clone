7c88871c7f4088fd00dc24c25fc67a3dd4561291
Bakhtiyor Khasanov <bkhasanov77@gmail.com>
2022-12-12 11:14:37 +0500

    [navbar] created navbar component

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route />
+      </Routes>
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+    fontFamily: { body: ['Roboto', 'sans-serif'] },
+    colors: {
+      blue: '#008DFF',
+      'blue-dark': '#0071CC',
+      white: '#fff',
+    },
+  },
+  plugins: [],
+}
